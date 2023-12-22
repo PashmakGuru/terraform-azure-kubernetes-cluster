1f2d58e797044d813d9120690d0a8adea9f82f50
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-12-23 02:56:28 +0330

    Add description to variables and validation for name

--- variables.tf
+++ variables.tf
@@ -1,11 +1,21 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "The name of the Azure Kubernetes Service (AKS) cluster. This name must be unique within the Azure resource group and should be descriptive of the cluster's purpose or application."
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]+$", var.name))
+    error_message = "The name must contain only lowercase letters, numbers, and hyphens."
+  }
 }
 
 variable "location" {
-  type = string
+  type        = string
+  default     = "West Europe"
+  description = "The Azure region where the AKS cluster will be deployed. Defaults to 'West Europe'. It's important to select a region close to the users for better performance."
 }
 
 variable "environment" {
-  type = string
+  type        = string
+  default     = "development"
+  description = "Specifies the deployment environment of the AKS cluster. Default is 'development'. This can be used to differentiate between stages such as development, staging, and production."
 }
